aa0cf7a4803a4844e171187cf8f58afbc93cf1ce
XYX <32657983+qpalwo@users.noreply.github.com>
2021-08-25 23:18:51 +0800

    gl test code

--- src/raw_entry.rs
+++ src/raw_entry.rs
@@ -5,18 +5,9 @@ use std::thread::sleep;
 use std::time::Duration;
 
 use android_logger::Config;
-use ash::{
-    Entry,
-    extensions::khr::Surface,
-    Instance, vk::{self, HANDLE},
-};
-use ash::vk::{Fence, SubmitInfo};
 use log::Level;
-use raw_window_handle::{HasRawWindowHandle, RawWindowHandle};
-use raw_window_handle::android::AndroidHandle;
+use raw_window_handle::RawWindowHandle;
 use skia_safe::{Budgeted, Canvas, gpu, ImageInfo};
-use skia_safe::gpu::vk::GetProcOf;
-use vk::Handle;
 
 pub type SizeT = raw::c_ulong;
 
@@ -24,20 +15,6 @@ pub type SizeT = raw::c_ulong;
 #[derive(Debug, Copy, Clone)]
 pub struct ANativeWindow {
     _unused: [u8; 0],
-}
-
-pub struct ANativeWindowWrapper {
-    ptr: *mut ANativeWindow,
-}
-
-unsafe impl HasRawWindowHandle for ANativeWindowWrapper {
-    fn raw_window_handle(&self) -> RawWindowHandle {
-        info!("handle1 {:}", self.ptr as u64);
-        return RawWindowHandle::Android(raw_window_handle::android::AndroidHandle {
-            a_native_window: self.ptr as *mut libc::c_void,
-            ..AndroidHandle::empty()
-        });
-    }
 }
 
 #[repr(C)]
@@ -56,7 +33,7 @@ pub struct ARect {
 }
 
 struct Window {
-    ash_graphics: AshGraphics,
+    // gl_context: GLContext<NativeGLContext>,
     render_context: gpu::DirectContext,
     width: i32,
     height: i32,
@@ -86,9 +63,6 @@ impl Window {
         let paint = Paint::new(&color, Some(&color_space));
         canvas.draw_rect(rect, &paint);
         surface.flush_and_submit();
-        unsafe {
-            self.ash_graphics.device.queue_submit(self.ash_graphics.queue_and_index.0, &vec![SubmitInfo::default()], Fence::null());
-        }
     }
 }
 
@@ -187,19 +161,54 @@ pub extern "C" fn on_window_created(activity: *mut ANativeActivity, window_ptr: 
     let app = App::get_app(activity);
     match app.window {
         None => {
-            // let a_native_window = unsafe { Box::from_raw(window_ptr) };
-            info!("handle 0 {:}", window_ptr as u64);
-            let ash_graphics = unsafe {
-                AshGraphics::new("rust_app", &ANativeWindowWrapper {
-                    ptr: window_ptr
-                })
-            };
-            app.window = Some(Window {
-                render_context: vk_init(ash_graphics.borrow()),
-                ash_graphics,
-                width: 0,
-                height: 0,
-            });
+            use surfman::{platform::android::{connection::Connection, device::NativeDevice, context::NativeContext}, GLApi,
+                          ContextAttributes, GLVersion, ContextAttributeFlags, SurfaceType, SurfaceAccess};
+
+            let connection = Connection::new().ok().unwrap();
+            let native_widget = connection.create_native_widget_from_rwh(RawWindowHandle::Android(raw_window_handle::android::AndroidHandle {
+                a_native_window: window_ptr as *mut libc::c_void,
+                ..raw_window_handle::android::AndroidHandle::empty()
+            })).ok().unwrap();
+
+            // let adapter = connection.create_low_power_adapter().unwrap();
+            // let mut device = connection.create_device(&adapter).unwrap();
+            //
+            // let context_attributes = ContextAttributes {
+            //     version: GLVersion::new(3, 0),
+            //     flags: ContextAttributeFlags::empty(),
+            // };
+            // let context_descriptor = device
+            //     .create_context_descriptor(&context_attributes)
+            //     .unwrap();
+            //
+            // let surface_type = SurfaceType::Widget { native_widget };
+            // let mut context = device.create_context(&context_descriptor, None).unwrap();
+
+            unsafe {
+                let device = connection
+                    .create_device_from_native_device(NativeDevice::current())
+                    .unwrap();
+                NativeContext::current().unwrap();
+                // let context = device
+                //     .create_context_from_native_context(NativeContext::current().unwrap())
+                //     .unwrap();
+                // let adapter = device.adapter();
+            }
+            // let surface = device
+            //     .create_surface(&context, SurfaceAccess::GPUOnly, surface_type)
+            //     .unwrap();
+            //
+            // device
+            //     .bind_surface_to_context(&mut context, surface)
+            //     .unwrap();
+            // device.make_context_current(&context).unwrap();
+
+
+            // app.window = Some(Window {
+            //     render_context: gpu::DirectContext::new_gl(None, None).unwrap(),
+            //     width: 0,
+            //     height: 0,
+            // });
         }
         _ => {}
     }
@@ -223,7 +232,7 @@ pub extern "C" fn on_window_resized(activity: *mut ANativeActivity, window_ptr: 
 pub extern "C" fn on_window_redraw(activity: *mut ANativeActivity, window_ptr: *mut ANativeWindow) {
     info!("Native window redraw, window = {:?}", window_ptr);
     let app = App::get_app(activity);
-    app.window.as_mut().unwrap().draw();
+    // app.window.as_mut().unwrap().draw();
 }
 
 #[no_mangle]
@@ -257,213 +266,5 @@ pub extern "C" fn ANativeActivity_onCreate(
         // callbacks.onInputQueueDestroyed = Some(on_input_queue_destroyed);
 
         App::init(activity);
-    }
-}
-
-
-fn vk_init(ash_graphics: &AshGraphics) -> gpu::DirectContext {
-    unsafe {
-        let context = {
-            let get_proc = |of| unsafe {
-                match ash_graphics.get_proc(of) {
-                    Some(f) => f as _,
-                    None => {
-                        println!("resolve of {} failed", of.name().to_str().unwrap());
-                        ptr::null()
-                    }
-                }
-            };
-
-            let backend_context = unsafe {
-                gpu::vk::BackendContext::new(
-                    ash_graphics.instance.handle().as_raw() as _,
-                    ash_graphics.physical_device.as_raw() as _,
-                    ash_graphics.device.handle().as_raw() as _,
-                    (
-                        ash_graphics.queue_and_index.0.as_raw() as _,
-                        ash_graphics.queue_and_index.1,
-                    ),
-                    &get_proc,
-                )
-            };
-            gpu::DirectContext::new_vulkan(&backend_context, None).unwrap()
-        };
-        context
-    }
-}
-
-
-pub struct AshGraphics {
-    pub entry: Entry,
-    pub instance: Instance,
-    pub physical_device: vk::PhysicalDevice,
-    pub device: ash::Device,
-    pub queue_and_index: (vk::Queue, usize),
-}
-
-impl Drop for AshGraphics {
-    fn drop(&mut self) {
-        unsafe {
-            self.device.device_wait_idle().unwrap();
-            self.device.destroy_device(None);
-            self.instance.destroy_instance(None);
-        }
-    }
-}
-
-impl AshGraphics {
-    pub fn vulkan_version() -> Option<(usize, usize, usize)> {
-        let entry = unsafe { Entry::new() }.unwrap();
-
-        let detected_version = entry.try_enumerate_instance_version().unwrap_or(None);
-
-        detected_version.map(|ver| {
-            (
-                vk::api_version_major(ver).try_into().unwrap(),
-                vk::api_version_minor(ver).try_into().unwrap(),
-                vk::api_version_patch(ver).try_into().unwrap(),
-            )
-        })
-    }
-
-    pub unsafe fn new(app_name: &str, a_native_window: &ANativeWindowWrapper) -> AshGraphics {
-        let entry = Entry::new().unwrap();
-
-        let minimum_version = vk::make_api_version(0, 1, 0, 0);
-
-        match a_native_window.raw_window_handle() {
-            RawWindowHandle::Android(handle) => {
-                info!("handle {:}", handle.a_native_window as u64);
-            }
-            _ => {}
-        }
-
-        let instance: Instance = {
-            let api_version = Self::vulkan_version()
-                .map(|(major, minor, patch)| {
-                    vk::make_api_version(
-                        0,
-                        major.try_into().unwrap(),
-                        minor.try_into().unwrap(),
-                        patch.try_into().unwrap(),
-                    )
-                })
-                .unwrap_or(minimum_version);
-            info!("api version {:}", api_version);
-
-            let surface_extensions = ash_window::enumerate_required_extensions(a_native_window).unwrap();
-            let mut extension_names_raw = surface_extensions
-                .iter()
-                .map(|ext| ext.as_ptr())
-                .collect::<Vec<_>>();
-
-
-            let app_name = CString::new(app_name).unwrap();
-            let layer_names: [&CString; 0] = []; // [CString::new("VK_LAYER_LUNARG_standard_validation").unwrap()];
-
-            let app_info = vk::ApplicationInfo::builder()
-                .application_name(&app_name)
-                .application_version(0)
-                .engine_name(&app_name)
-                .engine_version(0)
-                .api_version(api_version);
-
-            let layers_names_raw: Vec<*const raw::c_char> = layer_names
-                .iter()
-                .map(|raw_name| raw_name.as_ptr())
-                .collect();
-
-            let create_info = vk::InstanceCreateInfo::builder()
-                .application_info(&app_info)
-                .enabled_layer_names(&layers_names_raw)
-                .enabled_extension_names(&extension_names_raw);
-
-            entry
-                .create_instance(&create_info, None)
-                .expect("Failed to create a Vulkan instance.")
-        };
-
-        let surface = unsafe { ash_window::create_surface(&entry, &instance, a_native_window, None).unwrap() };
-
-        let (physical_device, queue_family_index) = {
-            let physical_devices = instance
-                .enumerate_physical_devices()
-                .expect("Failed to enumerate Vulkan physical devices.");
-
-            let surface_loader = Surface::new(&entry, &instance);
-
-            physical_devices
-                .iter()
-                .map(|physical_device| {
-                    instance
-                        .get_physical_device_queue_family_properties(*physical_device)
-                        .iter()
-                        .enumerate()
-                        .find_map(|(index, info)| {
-                            let supports_graphic_and_surface =
-                                info.queue_flags.contains(vk::QueueFlags::GRAPHICS)
-                                    && surface_loader
-                                    .get_physical_device_surface_support(
-                                        *physical_device,
-                                        index as u32,
-                                        surface,
-                                    )
-                                    .unwrap();
-                            if supports_graphic_and_surface {
-                                Some((*physical_device, index))
-                            } else {
-                                None
-                            }
-                        })
-                })
-                .find_map(|v| v)
-                .expect("Failed to find a suitable Vulkan device.")
-        };
-
-        let device: ash::Device = {
-            let features = vk::PhysicalDeviceFeatures::default();
-
-            let priorities = [1.0];
-
-            let queue_info = [vk::DeviceQueueCreateInfo::builder()
-                .queue_family_index(queue_family_index as _)
-                .queue_priorities(&priorities)
-                .build()];
-
-            let device_extension_names_raw = [];
-
-            let device_create_info = vk::DeviceCreateInfo::builder()
-                .queue_create_infos(&queue_info)
-                .enabled_extension_names(&device_extension_names_raw)
-                .enabled_features(&features);
-
-            instance
-                .create_device(physical_device, &device_create_info, None)
-                .unwrap()
-        };
-
-        let queue_index: usize = 0;
-        let queue: vk::Queue = device.get_device_queue(queue_family_index as _, queue_index as _);
-
-        AshGraphics {
-            queue_and_index: (queue, queue_index),
-            device,
-            physical_device,
-            instance,
-            entry,
-        }
-    }
-
-    pub unsafe fn get_proc(&self, of: gpu::vk::GetProcOf) -> Option<unsafe extern "system" fn()> {
-        match of {
-            gpu::vk::GetProcOf::Instance(instance, name) => {
-                let ash_instance = vk::Instance::from_raw(instance as _);
-                self.entry.get_instance_proc_addr(ash_instance, name)
-            }
-            gpu::vk::GetProcOf::Device(device, name) => {
-                let ash_device = vk::Device::from_raw(device as _);
-                self.instance.get_device_proc_addr(ash_device, name)
-            }
-        }
     }
 }
